ea6956c25f004423df253262736d8dca6e6b4c7e
Vadym Kazulkin <v.kazulkin@iplabs.de>
2024-12-25 22:20:40 +0100

    first try of aws lambda as graalvm native native image and micronaut 4.7

---
+++ micronaut-4.7-as-graalvm-native-image/src/main/java/software/amazonaws/example/product/controller/ProductAPIResourceWithDynamoDBRequestPriming.java
@@ -0,0 +1,33 @@
+package software.amazonaws.example.product.controller;
+
+import org.crac.Context;
+import org.crac.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.micronaut.crac.OrderedResource;
+import jakarta.inject.Singleton;
+import software.amazonaws.example.product.dao.ProductDao;
+
+
+@Singleton
+public class ProductAPIResourceWithDynamoDBRequestPriming implements OrderedResource  {
+
+	private final ProductDao productDao;
+	
+	private static final Logger logger = LoggerFactory.getLogger(ProductAPIResourceWithDynamoDBRequestPriming.class);
+	
+	public ProductAPIResourceWithDynamoDBRequestPriming(ProductDao productDao) {
+	    this.productDao = productDao;
+	}
+
+    @Override
+    public void beforeCheckpoint(Context<? extends Resource> context) throws Exception {
+    	logger.info("entered dynamo db requst priming before checkpoint method");
+    	this.productDao.getProduct("0");
+    }
+
+    @Override
+    public void afterRestore(Context<? extends Resource> context) throws Exception {
+    }
+}
