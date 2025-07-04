269e01a9a7b1c405abfe093fb164aa359bd7fee7
Vadym Kazulkin <v.kazulkin@iplabs.de>
2025-07-04 21:20:32 +0200

    added micronaut 4.9 on aws Lambda

---
+++ micronaut-4.9/src/main/java/software/amazonaws/example/product/controller/CreateProductController.java
@@ -0,0 +1,23 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+package software.amazonaws.example.product.controller;
+
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Put;
+import jakarta.inject.Inject;
+import software.amazonaws.example.product.dao.ProductDao;
+import software.amazonaws.example.product.entity.Product;
+
+@Controller
+public class CreateProductController {
+
+	@Inject
+	private ProductDao productDao;
+
+	@Put("/products")
+	public void createUpdateProduct(@Body Product product) {
+		productDao.putProduct(product);
+	}
+}
