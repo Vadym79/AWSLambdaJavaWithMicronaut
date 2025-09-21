334abbe5f335790f8982af4436af865f4f721595
Vadym Kazulkin <v.kazulkin@gmail.com>
2025-09-21 21:36:02 +0200

    some renamings

---
+++ micronaut-4.9-as-graalvm-native-image/src/main/java/software/amazonaws/example/product/handler/FunctionLambdaRuntime.java
@@ -0,0 +1,29 @@
+package software.amazonaws.example.product.handler;
+
+package example.micronaut;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import io.micronaut.function.aws.runtime.AbstractMicronautLambdaRuntime;
+import java.net.MalformedURLException;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import io.micronaut.core.annotation.Nullable;
+
+public class FunctionLambdaRuntime extends AbstractMicronautLambdaRuntime<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent, APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>
+{
+    public static void main(String[] args) {
+        try {
+        	System.out.println("args: "+args);
+            new FunctionLambdaRuntime().run(args);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    @Nullable
+    protected RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> createRequestHandler(String... args) {
+    	System.out.println("args1: "+args);
+        return new GetProductByIdHandler();
+    }
+}
